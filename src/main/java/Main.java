8ef70db942bbd5f9c8767a7f2f078e924288b0d2
LeriD567 <vlm050607@gmail.com>
2026-04-27 21:42:55 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,13 @@
 public class Main {
-  // Lỗi 1: Không thụt lề (indentation) đúng chuẩn
   public static void main(String[] args){
-// Lỗi 2: Thiếu khoảng trắng xung quanh toán tử
-    int X=10;
-// Lỗi 3: Đặt tên biến (X) viết hoa chữ cái đầu (sai quy tắc naming convention)
+    int x = 10;
     int y = 20;
 
-// Lỗi 4: Viết dính liền if và ngoặc nhọn
-    //Test lỗi
-    if(X>y){System.out.println("X lớn hơn y");}
+    if ( x > y ) {
+      System.out.println("X lớn hơn y");
+    }
     else{
-      System.out.println("y lớn hơn hoặc bằng X");
+      System.out.println("y lớn hơn hoặc bằng x");
     }
   }
 }
